9abcd4f769eda993becb82e5a2acacf8cb4efd34
haran seok <gkfks222@gmail.com>
2023-03-08 16:28:51 +0900

    vuetify 설치

--- vue_mobile/src/main.ts
+++ vue_mobile/src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import Router from './router'
 import { createI18n } from 'vue-i18n'
-import './style.css'
 import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 import i18n_ko from '@/i18n/ko.json'
+
+loadFonts()
 
 const messages = {
     ko: i18n_ko
@@ -15,9 +18,9 @@ const pinia = createPinia()
 const i18n = createI18n({
     locale: 'ko',
     messages
-});
-
-app.use(i18n)
-app.use(Router)
-app.use(pinia)
-app.mount('#app')
+})
+  app.use(i18n)
+  app.use(pinia)
+  app.use(router)
+  app.use(vuetify)
+  app.mount('#app')
