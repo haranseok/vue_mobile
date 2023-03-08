76cfdbafcb33b59efdbc655b4b2123bfcfa1afb4
haran seok <gkfks222@gmail.com>
2023-03-08 16:08:34 +0900

    i18n 설치

--- vue_mobile/src/main.ts
+++ vue_mobile/src/main.ts
@@ -1,12 +1,23 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import Router from './router'
+import { createI18n } from 'vue-i18n'
 import './style.css'
 import App from './App.vue'
+import i18n_ko from '@/i18n/ko.json'
+
+const messages = {
+    ko: i18n_ko
+}
 
 const app = createApp(App)
 const pinia = createPinia()
+const i18n = createI18n({
+    locale: 'ko',
+    messages
+});
 
+app.use(i18n)
 app.use(Router)
 app.use(pinia)
 app.mount('#app')
